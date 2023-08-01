4d2a6859f365daf8394ec246d884ad9020a52bde
liuyuanbo <1553592282@qq.com>
2023-08-01 17:57:04 +0800

    sentinel

--- sentinel-dashboard/sentinel-dashboard.bat
+++ sentinel-dashboard/sentinel-dashboard.bat
@@ -1,1 +1,2 @@
-java -Dserver.port=8718 -Dcsp.sentinel.dashboard.server=localhost:8718 -Dproject.name=sentinel-dashboard -jar sentinel-dashboard-1.8.6.jar
+:: [将自己注册到 Sentinel 控制台] -Dcsp.sentinel.dashboard.server=localhost:8718
+java -Dserver.port=8718 -Dproject.name=sentinel-dashboard -jar sentinel-dashboard-1.8.6.jar
